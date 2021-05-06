e87156188ef2d2053eb03e71618ed1b4a13ef23a
TechCiel <i@ciel.dev>
2021-05-06 17:08:05 +0800

    mul & mulw cmds

--- sim-core/src/commands/mul.cpp
+++ sim-core/src/commands/mul.cpp
@@ -1,7 +1,10 @@
 #include "../../include/command.h"
 
 // OP
-bool Command::mul() { return false; } // TODO
+bool Command::mul() {
+    reg->set(args[0], reg->get(args[1]) * reg->get(args[2]));
+    return true;
+}
 bool Command::mulh() { return false; } // TODO
 bool Command::mulhsu() { return false; } // TODO
 bool Command::mulhu() { return false; } // TODO
@@ -12,7 +15,8 @@ bool Command::remu() { return false; } // TODO
 
 //OP-32
 bool Command::mulw() {
-    reg->set(args[0], reg->get(args[1]) * reg->get(args[2]));
+    reg->set(args[0], reg->get(args[1])&0xffffffffll 
+        * reg->get(args[2])&0xffffffffll);
     return true;
 }
 bool Command::divw() { return false; } // TODO
