f055df8afd3fc3911ef2018c0080c5e7f93e8c78
TechCiel <i@ciel.dev>
2021-05-06 15:39:00 +0800

    op-imm & op-32-imm cmds

--- sim-core/src/commands/opimm.cpp
+++ sim-core/src/commands/opimm.cpp
@@ -5,20 +5,61 @@ bool Command::addi() {
     reg->set(args[0], reg->get(args[1]) + std::atoll(args[2].c_str()));
     return true;
 }
-bool Command::slti() { return false; } // TODO
-bool Command::sltiu() { return false; } // TODO
-bool Command::xori() { return false; } // TODO
-bool Command::ori() { return false; } // TODO
-bool Command::andi() { return false; } // TODO
-bool Command::slli() { return false; } // TODO
-bool Command::srli() { return false; } // TODO
-bool Command::srai() { return false; } // TODO
+bool Command::slti() {
+    if(reg->get(args[1]) < std::atoll(args[2].c_str())) reg->set(args[0], 1);
+    return true;
+}
+bool Command::sltiu() {
+    if (static_cast<unsigned long long>(reg->get(args[1]))
+        < static_cast<unsigned long long>(std::atoll(args[2].c_str()))
+        ) reg->set(args[0], 1);
+    return true;
+}
+bool Command::xori() {
+    reg->set(args[0], reg->get(args[1]) ^ std::atoll(args[2].c_str()));
+    return true;
+}
+bool Command::ori() {
+    reg->set(args[0], reg->get(args[1]) | std::atoll(args[2].c_str()));
+    return true;
+}
+bool Command::andi() {
+    reg->set(args[0], reg->get(args[1]) & std::atoll(args[2].c_str()));
+    return true;
+}
+bool Command::slli() {
+    reg->set(args[0], reg->get(args[1]) << std::atoll(args[2].c_str())&0b111111);
+    return true;
+}
+bool Command::srli() {
+    reg->set(args[0], static_cast<unsigned long long>(reg->get(args[1])) 
+        >> std::atoll(args[2].c_str())&0b111111);
+    return true;
+}
+bool Command::srai() {
+    reg->set(args[0], reg->get(args[1]) >> std::atoll(args[2].c_str())&0b111111);
+    return true;
+}
 
 // OP-IMM-32
 bool Command::addiw() {
-    reg->set(args[0], (int)reg->get(args[1]) + std::atoll(args[2].c_str()));
+    reg->set(args[0], (int)(reg->get(args[1])&0xffffffffll)
+        + (int)std::atoll(args[2].c_str()));
     return true;
 }
-bool Command::slliw() { return false; } // TODO
-bool Command::srliw() { return false; } // TODO
-bool Command::sraiw() { return false; } // TODO
+bool Command::slliw() {
+    reg->set(args[0], (int)(reg->get(args[1])&0xffffffffll)
+        << std::atoll(args[2].c_str())&0b11111);
+    return true;
+}
+bool Command::srliw() {
+    reg->set(args[0],
+        (unsigned int)(static_cast<unsigned long long>(reg->get(args[1]))&0xffffffffll)
+        >> std::atoll(args[2].c_str())&0b111111);
+    return true;
+}
+bool Command::sraiw() {
+    reg->set(args[0], (int)(reg->get(args[1])&0xffffffffll)
+        >> std::atoll(args[2].c_str())&0b111111);
+    return true;
+}
