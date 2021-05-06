98ab9ae2643edad7608a2d53a4167ab0ca3ec015
TechCiel <i@ciel.dev>
2021-05-06 17:16:44 +0800

    lui & auipc cmds

--- sim-core/src/commands/misc.cpp
+++ sim-core/src/commands/misc.cpp
@@ -1,10 +1,19 @@
 #include "../../include/command.h"
 
 // LUI
-bool Command::lui() { return false; } // TODO
+bool Command::lui() {
+    long long data = std::stoi(args[1].c_str()+2, nullptr, 16)<<12;
+    if(data & (1<<31)) data |= 0xffffffff00000000ll;
+    reg->set(args[0], data);
+    return true;
+}
 
 // AUIPC
-bool Command::auipc() { return false; } // TODO
+bool Command::auipc() {
+    long long data = std::stoi(args[1].c_str()+2, nullptr, 16)<<12;
+    if(data & (1<<31)) data |= 0xffffffff00000000ll;
+    reg->set(args[0], (*pc)+data);
+}
 
 // MEM-MISC
 bool Command::fence() { return true; }
