6e03d36740b34b14597f5df0c270854835d24b97
TechCiel <i@ciel.dev>
2021-05-06 15:50:28 +0800

    some pseudo cmds

--- sim-core/src/commands/pseudo.cpp
+++ sim-core/src/commands/pseudo.cpp
@@ -1,23 +1,68 @@
 #include "../../include/command.h"
 
 // pseudo
-bool Command::nop() { return true; }
+bool Command::nop() {
+    return true;
+    }
 bool Command::li() {
     reg->set(args[0], std::atoll(args[1].c_str()));
     return true;
 }
 bool Command::mv() {
-    reg->set(args[0], reg->get(args[1]));
-    return true;
+    args.resize(3);
+    args[2] = "0";
+    name = "addi";
+    return addi();
 }
-bool Command::not_() { return false; } // TODO
-bool Command::neg() { return false; } // TODO
-bool Command::negw() { return false; } // TODO
+bool Command::not_() {
+    args.resize(3);
+    args[2] = "-1";
+    name = "xori";
+    return xori();
+}
+bool Command::neg() {
+    args.resize(3);
+    args[2] = args[1];
+    args[1] = "zero";
+    name = "sub";
+    return sub();
+}
+bool Command::negw() {
+    args.resize(3);
+    args[2] = args[1];
+    args[1] = "zero";
+    name = "subw";
+    return subw();
+}
 bool Command::sextw() {
-    reg->set(args[0], (int)reg->get(args[1]));
-    return true;
+    args.resize(3);
+    args[2] = "0";
+    name = "addiw";
+    return addiw();
 }
-bool Command::seqz() { return false; } // TODO
-bool Command::snez() { return false; } // TODO
-bool Command::sltz() { return false; } // TODO
-bool Command::sgtz() { return false; } // TODO
+bool Command::seqz() {
+    args.resize(3);
+    args[2] = "1";
+    name = "sltiu";
+    return sltiu();
+}
+bool Command::snez() {
+    args.resize(3);
+    args[2] = args[1];
+    args[1] = "zero";
+    name = "sltu";
+    return sltu();
+}
+bool Command::sltz() {
+    args.resize(3);
+    args[2] = "zero";
+    name = "slt";
+    return slt();
+}
+bool Command::sgtz() {
+    args.resize(3);
+    args[2] = args[1];
+    args[1] = "zero";
+    name = "slt";
+    return slt();
+}
