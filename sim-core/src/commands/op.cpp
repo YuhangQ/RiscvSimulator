04f04e3d2d1965912ccd531001dc2aff84cd8424
TechCiel <i@ciel.dev>
2021-05-06 15:21:43 +0800

    op & op-32 cmds

--- sim-core/src/commands/op.cpp
+++ sim-core/src/commands/op.cpp
@@ -1,23 +1,74 @@
 #include "../../include/command.h"
 
 // OP
-bool Command::add() { return false; } // TODO
-bool Command::sub() { return false; } // TODO
-bool Command::slt() { return false; } // TODO
-bool Command::sltu() { return false; } // TODO
-bool Command::xor_() { return false; } // TODO
-bool Command::or_() { return false; } // TODO
-bool Command::and_() { return false; } // TODO
-bool Command::sll() { return false; } // TODO
-bool Command::srl() { return false; } // TODO
-bool Command::sra() { return false; } // TODO
-
-// OP-32
-bool Command::addw() {
+bool Command::add() {
     reg->set(args[0], reg->get(args[1]) + reg->get(args[2]));
     return true;
 }
-bool Command::subw() { return false; } // TODO
-bool Command::sllw() { return false; } // TODO
-bool Command::srlw() { return false; } // TODO
-bool Command::sraw() { return false; } // TODO
+bool Command::sub() {
+    reg->set(args[0], reg->get(args[1]) - reg->get(args[2]));
+    return true;
+}
+bool Command::slt() {
+    if (reg->get(args[1]) < reg->get(args[2])) reg->set(args[0], 1);
+    return true;
+}
+bool Command::sltu() {
+    if (static_cast<unsigned long long>(reg->get(args[1]))
+        < static_cast<unsigned long long>(reg->get(args[2]))
+        ) reg->set(args[0], 1);
+    return true;
+}
+bool Command::xor_() {
+    reg->set(args[0], reg->get(args[1]) ^ reg->get(args[2]));
+    return true;
+}
+bool Command::or_() {
+    reg->set(args[0], reg->get(args[1]) | reg->get(args[2]));
+    return true;
+}
+bool Command::and_() {
+    reg->set(args[0], reg->get(args[1]) & reg->get(args[2]));
+    return true;
+}
+bool Command::sll() {
+    reg->set(args[0], reg->get(args[1]) << reg->get(args[2])&0b111111);
+    return true;
+}
+bool Command::srl() {
+    reg->set(args[0], static_cast<unsigned long long>(reg->get(args[1])) 
+        >> reg->get(args[2])&0b111111);
+    return true;
+}
+bool Command::sra() {
+    reg->set(args[0], reg->get(args[1]) >> reg->get(args[2])&0b111111);
+    return true;
+}
+
+// OP-32
+bool Command::addw() {
+    reg->set(args[0], ((int)(reg->get(args[1])&0xffffffffll)
+        + (int)(reg->get(args[2])&0xffffffffll)));
+    return true;
+}
+bool Command::subw() {
+    reg->set(args[0], ((int)(reg->get(args[1])&0xffffffffll)
+        - (int)(reg->get(args[2])&0xffffffffll)));
+    return true;
+}
+bool Command::sllw() {
+    reg->set(args[0], (int)(reg->get(args[1])&0xffffffffll)
+        << reg->get(args[2])&0b11111);
+    return true;
+}
+bool Command::srlw() {
+    reg->set(args[0],
+        (unsigned int)(static_cast<unsigned long long>(reg->get(args[1]))&0xffffffffll)
+        >> reg->get(args[2])&0b111111);
+    return true;
+}
+bool Command::sraw() {
+    reg->set(args[0], (int)(reg->get(args[1])&0xffffffffll)
+        >> reg->get(args[2])&0b111111);
+    return true;
+}
